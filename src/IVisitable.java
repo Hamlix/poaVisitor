71b0a63e52d33568283e868c92ca820cba033af6
Hamlix <nathanaelle@famille-viller.com>
2016-11-07 16:17:23 +0100

    Création du squelette des interfaces

---
+++ src/IVisitable.java
@@ -0,0 +1,8 @@
+/**
+ * Created by galax on 07/11/2016.
+ */
+public interface IVisitable {
+
+    public void accept(IVisitor visiteur);
+}
+
---
+++ src/IVisitor.java
@@ -0,0 +1,9 @@
+/**
+ * Created by galax on 07/11/2016.
+ */
+public interface IVisitor {
+    public void visit (Client c);
+    public void visit (Commande o);
+    public void visit (Ligne l);
+    public void afficherCommandes();
+}
