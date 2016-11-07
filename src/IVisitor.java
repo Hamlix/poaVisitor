ada18c6d71092ac9b607bdd9c8302f9c338087dc
Hamlix <nathanaelle@famille-viller.com>
2016-11-07 17:14:23 +0100

    Rajout d'une méthode, supp de méthode obsolète

--- src/IVisitor.java
+++ src/IVisitor.java
@@ -5,5 +5,5 @@ public interface IVisitor {
     public void visit (Client c);
     public void visit (Commande o);
     public void visit (Ligne l);
-    public void afficherCommandes();
+    public void visit (GroupeClient gc);
 }
